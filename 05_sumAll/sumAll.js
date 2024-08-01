2485b57d09362b9eab155f3a9d244592afc95e32
danicaracow <79090619+danicaracow@users.noreply.github.com>
2024-08-01 20:04:50 +0200

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let result = 0;
 
+    if(num1<0 || num2<0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }
+
+    if (num1 < num2){
+        for (i = num1; i<=num2; i++){
+            result = result + i;
+        }
+    }
+    
+    if (num1 > num2){
+        for (i = num2; i<=num1; i++){
+            result = result + i;
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
