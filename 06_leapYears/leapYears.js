b09212a454ee0c06ccb46b1bac733c210fc440ce
danicaracow <79090619+danicaracow@users.noreply.github.com>
2024-08-01 20:15:40 +0200

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year%400 === 0){
+        console.log("400");
+        return true;
+    }
+    else if (year%100 === 0){
+        console.log("100");
+        return false;
+    }
+    else if (year%4 === 0){
+        console.log("4");
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
