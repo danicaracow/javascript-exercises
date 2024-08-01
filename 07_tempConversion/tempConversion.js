c4345e938b5ca722e412828927d0d2369ac85e9b
danicaracow <79090619+danicaracow@users.noreply.github.com>
2024-08-01 20:29:49 +0200

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let resultTemp;
+
+  resultTemp = Math.round(((temp-32)*(5/9)*10))/10;
+
+  return resultTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let resultTemp;
+
+  resultTemp = Math.round(((temp*(9/5)+32))*10)/10;
+
+  return resultTemp;
 };
 
 // Do not edit below this line
