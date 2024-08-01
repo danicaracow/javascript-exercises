3724bcd9a88bf733883705fa3dec57f172e92805
danicaracow <79090619+danicaracow@users.noreply.github.com>
2024-08-01 19:47:13 +0200

    Complete exercise 3

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...deleteElem) {
+
+    for (const elem of [...array]) {
+        if (deleteElem.includes(elem)) {
+            array.splice(array.indexOf(elem), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
