cc192969e2b634cf6ec4ed507cb3079df90256a8
danicaracow <79090619+danicaracow@users.noreply.github.com>
2024-08-01 16:35:49 +0200

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    reversedString = "";
 
+    for(i = str.length-1; i>=0; i--){
+        reversedString = reversedString + str[i];
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
