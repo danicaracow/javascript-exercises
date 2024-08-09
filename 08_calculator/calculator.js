726f4026a6c2238ce213c1e79ff134f31874f0e1
danicaracow <79090619+danicaracow@users.noreply.github.com>
2024-08-09 17:09:35 +0200

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0){
+    return 1;
+  }
+  else if (a > 0){
+    let total = 1;
+    for (i=1; i<=a; i++){
+      total = total * i;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
