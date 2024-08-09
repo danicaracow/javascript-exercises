a8d0f73f39b44ab89a0269badf812f5ec454e299
danicaracow <79090619+danicaracow@users.noreply.github.com>
2024-08-09 18:41:43 +0200

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibonacci = [1, 1];
+    num = Number(num);
 
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
+    if (num === 1 || num === 2) return 1;
+
+    for(i=2; i<num; i++){
+        fibonacci.push(fibonacci[i-2]+fibonacci[i-1]);
+    }
+
+    return fibonacci[fibonacci.length-1];
 };
 
 // Do not edit below this line
