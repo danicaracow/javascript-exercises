eb11d1e006d5bddf7def5ba9f7686da7bbb8c9db
danicaracow <79090619+danicaracow@users.noreply.github.com>
2024-08-10 11:34:57 +0200

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    //Add current date to people without yearOfDeath
+    const completed = array.map((person) => {
+        if(!("yearOfDeath" in person)){
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        return person;
+    })
 
+    //Order people by age (older first)
+    const ordered = completed.sort((a, b) => {
+        if(a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth){
+            return -1;
+        }
+        else{
+            return 1;
+        }
+    })
+
+    //Return oldest person
+    return ordered[0];
 };
 
 // Do not edit below this line
