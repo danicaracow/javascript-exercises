98ad2b8c6166ffc6feda913082b2342da66ddede
danicaracow <79090619+danicaracow@users.noreply.github.com>
2024-08-10 10:34:21 +0200

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(data) {
+    array = [];
+    data.forEach((obj) => {
+        array.push(obj.title);
+    })
+    return array;
 };
 
 // Do not edit below this line
