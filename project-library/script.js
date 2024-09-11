10d8712a7827c5b438fcf8a238ef0ef0a3eee745
danicaracow <79090619+danicaracow@users.noreply.github.com>
2024-09-11 23:07:57 +0200

    Add dialog, form input and book info display

--- project-library/script.js
+++ project-library/script.js
@@ -5,24 +5,58 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-const bookInput = document.querySelector("#book-input");
 const submitBook = document.querySelector("#submit-book");
+const newBook = document.querySelector("#new-book");
+const dialog = document.querySelector("dialog");
+const form = document.querySelector("#book-form");
 
-console.log(bookInput);
-submitBook.addEventListener("click", addBookToLibrary);
+const inputTitle = document.querySelector("#title");
+const inputAuthor = document.querySelector("#author");
+const inputPages = document.querySelector("#pages");
+const inputRead = document.querySelector("#read");
 
-function addBookToLibrary() {
-    [title, author, pages, read] = bookInput.value.split(",");
-    bookInput.value = "";
-    const book = new Book(title, author, pages, read);
+
+
+const cardsContainer = document.querySelector(".container");
+
+// submitBook.addEventListener("click", (event) => addBookToLibrary(event));
+newBook.addEventListener("click", () => dialog.showModal());
+
+form.addEventListener("submit", function(e) {
+    addBookToLibrary(e);
+});
+
+function addBookToLibrary(event) {
+    event.preventDefault();  // Prevents the default form submission behavior
+    dialog.close();
+    const book = new Book(inputTitle.value, inputAuthor.value, inputPages.value, inputRead.checked);
     myLibrary.push(book);
 
     displayBooks(myLibrary);
 }
 
 function displayBooks(library){
+    // library.forEach(element => {
+    //     console.log(element.title + "," + element.author + "," + element.pages + "," + element.read);
+    // });
+
     library.forEach(element => {
-        console.log(element.title + "," + element.author + "," + element.pages + "," + element.read);
+        const card = document.createElement("div");
+        const testTitle = document.createElement("p");
+        const testAuthor = document.createElement("p");
+        const testPages = document.createElement("p");
+        const testRead = document.createElement("p");
+
+        cardsContainer.appendChild(card);
+        card.appendChild(testTitle);
+        testTitle.textContent = element.title;
+        card.appendChild(testAuthor);
+        testAuthor.textContent = element.author;
+        card.appendChild(testPages);
+        testPages.textContent = element.pages;
+        card.appendChild(testRead);
+        testRead.textContent = element.read;
+
     });
 }
 
@@ -35,4 +69,4 @@ const book1 = {
 
 const myLibrary = [book1];
 
-displayBooks(myLibrary);
+// displayBooks(myLibrary);
